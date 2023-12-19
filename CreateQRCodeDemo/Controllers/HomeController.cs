c2ab8af525b710e29c02bd1dfef79e6d20b220b5
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-19 16:36:00 +0700

    update code

--- CreateQRCodeDemo/Controllers/HomeController.cs
+++ CreateQRCodeDemo/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using CreateQRCodeDemo.Models;
+using IronBarCode;
+using IronSoftware.Drawing;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,17 +10,38 @@ namespace CreateQRCodeDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult Index(QRViewModel model)
+        {
+            //string url = @"http://103.160.5.59:8001/CSSurvey/index?storenumber=TSHCMNTPSS05";
+            AnyBitmap qrlogo = AnyBitmap.FromFile(@"C:\Users\nguye\source\repos\RenderQRcode\RenderQRcode\wwwroot\logo.png");
+            QRCodeLogo logo = new QRCodeLogo(qrlogo, 0, 0, 20f);
+            GeneratedBarcode barcode = QRCodeWriter.CreateQrCodeWithLogo(model.Url, logo, 250);//QRCodeWriter.CreateQrCode(url, 500);
+            //barcode.AddBarcodeValueTextAboveBarcode();
+            barcode.SetMargins(10);
+            //barcode.ChangeBackgroundColor(Color.Black);
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "QRCode");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string qr = Guid.NewGuid().ToString() + ".png";
+            string filePath = Path.Combine(_webHostEnvironment.WebRootPath, qr);
+            barcode.SaveAsPng(filePath);
+            ViewBag.url = qr;
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ CreateQRCodeDemo/Models/QRViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CreateQRCodeDemo.Models
+{
+    public class QRViewModel
+    {
+        public string Url { get; set; }
+        public IFormFile Logo { get; set; }
+    }
+}
