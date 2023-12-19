6d4ea6abf58118b9d340c5b853df91a29d8e7bba
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-19 14:38:59 +0700

    Add multiple language

---
+++ MultipleLanguageDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultipleLanguageDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
