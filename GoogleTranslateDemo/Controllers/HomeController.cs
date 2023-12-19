01f76074d59e4df0eb81ef252b7b76818d665d8e
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-19 16:52:13 +0700

    update code

--- GoogleTranslateDemo/Controllers/HomeController.cs
+++ GoogleTranslateDemo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using GoogleTranslateDemo.Models;
+﻿using Google.Cloud.Translation.V2;
+using GoogleTranslateDemo.ModelHelpers;
+using GoogleTranslateDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,42 @@ namespace GoogleTranslateDemo.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly GoogleTransalationHelper _googleTransalationHelper;
+        public HomeController(ILogger<HomeController> logger, GoogleTransalationHelper googleTransalationHelper)
         {
             _logger = logger;
+            _googleTransalationHelper = googleTransalationHelper;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ResultObject Tranlation([FromBody] TranslationViewModel model)
+        {
+            ResultObject resultObject = new ResultObject() { Ok = false };
+            if (model != null)
+            {
+                //System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @".\Resource\projecttranslateapi-397007-4c3ee3daa0e2.json");
+                //var client = TranslationClient.Create();
+                //if (model.Language == "VietNamese")
+                //{
+                //    TranslationResult result = client.TranslateText(model.Text, LanguageCodes.English);
+                //    resultObject.Ok = true;
+                //    resultObject.Content = result.TranslatedText;
+                //    return resultObject;
+                //}
+                //else if(model.Language == "English")
+                //{
+                //    TranslationResult result = client.TranslateText(model.Text, LanguageCodes.Vietnamese);
+                //    resultObject.Ok = true;
+                //    resultObject.Content = result.TranslatedText;
+                //    return resultObject;
+                //}
+                return _googleTransalationHelper.LanguageTranslation(model);
+            }
+            return resultObject;
         }
 
         public IActionResult Privacy()
