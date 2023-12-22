a776693cef94fdf7329c8f42e86c3d048c64910f
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-22 10:39:48 +0700

    update code

--- PopUpDemo/Controllers/ProductionController.cs
+++ PopUpDemo/Controllers/ProductionController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using PopUpDemo.Helpers;
 using PopUpDemo.Models;
 
@@ -78,6 +81,26 @@ namespace PopUpDemo.Controllers
             _homeViewModelHelper.DeleteItemByID(id);
             return RedirectToAction("Index");
 
+        }
+        public string RenderRazorViewToString(Controller controller, string viewName, object model = null)
+        {
+            controller.ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                IViewEngine viewEngine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, false);
+
+                ViewContext viewContext = new ViewContext(
+                    controller.ControllerContext,
+                    viewResult.View,
+                    controller.ViewData,
+                    controller.TempData,
+                    sw,
+                    new HtmlHelperOptions()
+                );
+                viewResult.View.RenderAsync(viewContext);
+                return sw.GetStringBuilder().ToString();
+            }
         }
     }
 }
--- PopUpDemo/wwwroot/js/site.js
+++ PopUpDemo/wwwroot/js/site.js
@@ -2,3 +2,49 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function SubmitForm(form) {
+    $.ajax({
+        type: 'POST',
+        url: form.action,
+        data: new FormData(form),
+        contentType: false,
+        processData: false,
+        success: function (res) {
+            if (res.isValid) {
+                var myModal = document.getElementById('btnModel');
+                $('#popupMessage').text(res.message);
+                myModal.click();
+                setTimeout(() => {
+                    window.location.href = '/';
+
+                }, 15000);
+            }
+            else if (res.message) {
+                var myModal = document.getElementById('btnModel');
+                $('#popupMessage').text(res.message);
+                myModal.click();
+                setTimeout(() => {
+                    window.location.href = '/';
+
+                }, 15000);
+            }
+            else if (res.messageNotification) {
+                $('#formSV').empty();
+                $('#formSV').append(res.html);
+                var myModalNotification = document.getElementById('btnModelNotification');
+                $('#popupNotifiacation').text(res.messageNotification);
+                myModalNotification.click();
+
+            }
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+    return false;
+}
+
+//var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+//    keyboard: false
+//})
+//myModal.show();
