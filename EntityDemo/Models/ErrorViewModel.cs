59fc3dea9e5a7bd9d92e60b2ebfc55d54afa30c1
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-22 14:20:06 +0700

    update code

---
+++ EntityDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EntityDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
