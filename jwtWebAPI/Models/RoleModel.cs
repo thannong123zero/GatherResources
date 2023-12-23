33a3a751791382f702a4a2f971cd15811c5367d8
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-23 10:03:08 +0700

    update code

---
+++ jwtWebAPI/Models/RoleModel.cs
@@ -0,0 +1,8 @@
+﻿namespace jwtWebAPI.Models
+{
+    public class RoleModel
+    {
+        public string ID { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ jwtWebAPI/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace jwtWebAPI.Models
+{
+    public class UserModel
+    {
+        public string ID { get; set; }
+        public string FullName { get; set; }
+        public int Sex { get; set; }
+        public DateTime BrithDay { get; set; }
+    }
+}
