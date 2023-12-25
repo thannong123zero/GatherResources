c2156910df84d0e3274d382b7c672d622f14facf
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-25 11:19:52 +0700

    update code

--- jwtWebAPI/Controllers/AccountController.cs
+++ jwtWebAPI/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
 ﻿using jwtWebAPI.Helpers;
 using jwtWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace jwtWebAPI.Controllers
 {
@@ -9,26 +12,42 @@ namespace jwtWebAPI.Controllers
     public class AccountController : ControllerBase
     {
         private readonly MasterModelHelper _masterModelHelper;
-        public AccountController(MasterModelHelper masterModelHelper)
+        private readonly JwtConfiguration _jwtConfiguration;
+        public AccountController(MasterModelHelper masterModelHelper, JwtConfiguration jwtConfiguration)
         {
             _masterModelHelper = masterModelHelper;
+            _jwtConfiguration = jwtConfiguration;
         }
 
         [HttpPost(Name = "Login")]
-        public DataObjectResult Login(LoginModel model)
+        public IActionResult Login(LoginModel model)
         {
-            DataObjectResult dataObjectResult = new DataObjectResult();
+
+            IActionResult response = Unauthorized();
             UserModel user = _masterModelHelper.Login(model);
 
             if (user != null)
             {
-                dataObjectResult.Ok = true;
-                dataObjectResult.Content = user;
-                return dataObjectResult;
+                var tokenString = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenString });
             }
 
-            dataObjectResult.Message = "UserName or password is isvalid!";
-            return dataObjectResult;
+            return response;
         }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfiguration.Key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_jwtConfiguration.Issuer,
+              _jwtConfiguration.Issuer,
+              null,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
     }
 }
--- jwtWebAPI/Program.cs
+++ jwtWebAPI/Program.cs
@@ -1,7 +1,10 @@
 
 using jwtWebAPI.Helpers;
 using jwtWebAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace jwtWebAPI
 {
@@ -18,9 +21,27 @@ namespace jwtWebAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddSingleton<MasterModelHelper>();
-            var jwtConfig = builder.Configuration.GetSection("JwtConfig");
+
+            var jwtConfig = builder.Configuration.GetSection("AppConfig");
             JwtConfiguration jwtConfiguration = jwtConfig.Get<JwtConfiguration>();
             builder.Services.AddSingleton(jwtConfiguration);
+
+            //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //.AddJwtBearer(options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        ValidateIssuer = true,
+            //        ValidateAudience = true,
+            //        ValidateLifetime = true,
+            //        ValidateIssuerSigningKey = true,
+            //        ValidIssuer = jwtConfiguration.Issuer,
+            //        ValidAudience = jwtConfiguration.Issuer,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration.Key))
+            //    };
+            //});
+
+
 
             var app = builder.Build();
 
