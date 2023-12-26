dd2e4abb55e680ffcc1159cdc9b59a4c0cf690f7
Mr Zero <nguyenhoangtai2k@gmail.com>
2023-12-26 11:10:14 +0700

    update document

--- Lipstick/Models/Explain.cs
+++ Lipstick/Models/Explain.cs
@@ -2,6 +2,53 @@
 {
     public class Explain
     {
+        #region section 1
+        /* 
+          
+            Hello, Đây là series về asp.net core mvc (.Net 7)
+           
+            Series này được thực hiện theo phong cách: Kết hợp lý thuyết và thực hành.
+            => Nhầm tránh việc các bạn viết code mà không hiểu đoạn code nó hoạt động như thế nào!
+           
+            Yêu cầu:
+            + Đã học về lập OPP
+            + Đã biết về ngôn ngữ lập trình.
+            + Hiểu về cơ sở dữ liệu
+          
+         */
+        #endregion
+        #region Section 2
+        /*
+        
+            Trước khi thưởng thức món chính thì chúng ta sẽ dùng món khai vị trước nha !   
+        
+            Chúng ta sẽ tìm hiểu sơ bộ về asp.net là gì ?
+            Nó có thể làm được gì?
+            => Ref: https://dotnet.microsoft.com/en-us/apps/aspnet
+        
+            Như chúng ta đã biết, Một trang web động thì sẽ có:
+            + Giao diện người dùng (font end).
+            + Xử lý logic và tương tác với csdl ( back end).
 
+            Vì vậy, đối với website lớn thì người ta sẽ chia ra làm 2 dự án:
+            + Web UI 
+            + Web API
+
+            Thông thường sẽ có 2 sự lựa chọn về việc phát triển web UI, đó là:
+            + Multiple page: Ví dụ điển hình là: Asp .Net core MVC ( có nhiều công nghệ khác).
+            + Single page: Ví dụ điển hình là: Reactjs (có nhiều công nghệ khác).
+
+            => Để biết thêm thông tin về sự khác nhau này thì search key word: 
+            "Difference between single page and multiple page application"
+        
+            Trong ASP.Net sẽ hỗ trợ chúng ta công nghệ làm web UI như sau:
+            + ASP.NET Core Blazor
+            + ASP.NET Core Razor Pages
+            + ASP.NET Core MVC
+            + ASP.NET Core Single Page Applications (SPA) with frontend JavaScript frameworks
+            => Ref: https://learn.microsoft.com/en-us/aspnet/core/tutorials/choose-web-ui?view=aspnetcore-7.0
+
+         */
+        #endregion
     }
 }
