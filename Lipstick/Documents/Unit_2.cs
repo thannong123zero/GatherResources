b9b126575a109e794279be7bcb3fc8149fb387ca
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-01-25 12:59:56 +0700

    update code

---
+++ Lipstick/Documents/Unit_2.cs
@@ -0,0 +1,15 @@
+﻿namespace Lipstick.Documents
+{
+    public class Unit_2
+    {
+        #region Building Views
+        /*
+         * Bước 1: xây dựng layout
+         * 
+         * Bước 2: xây dựng các view
+         * 
+         * 
+         */
+        #endregion
+    }
+}
