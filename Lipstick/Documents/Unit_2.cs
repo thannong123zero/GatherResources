9cc458f514a9256107943710c02d546762add1d9
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-01 18:57:08 +0700

    update code

--- Lipstick/Documents/Unit_2.cs
+++ Lipstick/Documents/Unit_2.cs
@@ -4,9 +4,13 @@
     {
         #region Building Views
         /*
-         * Bước 1: xây dựng layout
+         * Bước 1: Chung ta se phan tich du an. Xem minh se co bao nhieu trang
          * 
-         * Bước 2: xây dựng các view
+         * Bước 2: Tim ra cac diem chung cua du an roi chung ta se tach playout
+         * 
+         * Buoc 3: Tim hieu ve View va Partial View
+         * 
+         * Buoc 4: Tim hieu cach su dung razerpage
          * 
          * 
          * 
