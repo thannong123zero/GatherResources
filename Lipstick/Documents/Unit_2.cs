e6b9cd8f3f34e9c1dc1ee848e16d117206a252f3
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-09 14:38:42 +0700

    update code

--- Lipstick/Documents/Unit_2.cs
+++ Lipstick/Documents/Unit_2.cs
@@ -4,14 +4,17 @@
     {
         #region Building Views
         /*
-         * Phan 1: Chung ta se phan tich du an. Xem minh se co bao nhieu trang
+         * 
+         * 
+         * 
+         * Chung ta se phan tich du an. Xem minh se co bao nhieu trang
          * 
          * Vi sao phai phan tich du an?
          * Sau khi chung ta nhan duoc ban thiet ke tu file xd or figma
          * Chung ta se bat dau chuyen doi file thiet ke thanh code 
          * Tiep theo chung ta thuoc hien xu ly logic
          * 
-         * Phan 2: Tim hieu ve View va html taghelper
+         * Phan 1: Tim hieu ve View va html taghelper
          * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/tag-helpers/built-in/?view=aspnetcore-8.0
          * 
          * What ?
@@ -23,7 +26,7 @@
          * 
          * 
          * 
-         * Phan 3: Tim ra cac diem chung cua du an roi chung ta se tach playout
+         * Phan 2: Tim ra cac diem chung cua du an roi chung ta se tach playout
          * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/layout?view=aspnetcore-8.0
          * 
          * What ?
@@ -32,7 +35,7 @@
          * When ?
          * 
          * 
-         * Phan 4: Tim hieu ve Partial View, view Component
+         * Phan 3: Tim hieu ve Partial View, view Component
          * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/partial?view=aspnetcore-8.0
          * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/view-components?view=aspnetcore-8.0
          * 
