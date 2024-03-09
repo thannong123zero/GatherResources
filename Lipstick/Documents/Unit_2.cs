ffe8408d9de7260b5b9bd22efc824a29074cdca0
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-09 12:02:45 +0700

    update code

--- Lipstick/Documents/Unit_2.cs
+++ Lipstick/Documents/Unit_2.cs
@@ -4,13 +4,42 @@
     {
         #region Building Views
         /*
-         * Bước 1: Chung ta se phan tich du an. Xem minh se co bao nhieu trang
+         * Phan 1: Chung ta se phan tich du an. Xem minh se co bao nhieu trang
          * 
-         * Bước 2: Tim ra cac diem chung cua du an roi chung ta se tach playout
+         * Vi sao phai phan tich du an?
+         * Sau khi chung ta nhan duoc ban thiet ke tu file xd or figma
+         * Chung ta se bat dau chuyen doi file thiet ke thanh code 
+         * Tiep theo chung ta thuoc hien xu ly logic
          * 
-         * Buoc 3: Tim hieu ve View va Partial View
          * 
-         * Buoc 4: Tim hieu cach su dung razerpage
+         * 
+         * Phan 2: Tim ra cac diem chung cua du an roi chung ta se tach playout
+         * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/layout?view=aspnetcore-8.0
+         * 
+         * What ?
+         * Why ?
+         * How ?
+         * When ?
+         * 
+         * 
+         * Phan 3: Tim hieu ve View va Partial View, view Component
+         * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/partial?view=aspnetcore-8.0
+         * 
+         * What ?
+         * Why ?
+         * How ?
+         * When ?
+         * 
+         * 
+         * 
+         * Phan 4: Tim hieu ve html taghelper
+         * ref: https://learn.microsoft.com/en-us/aspnet/core/mvc/views/tag-helpers/built-in/?view=aspnetcore-8.0
+         * 
+         * What ?
+         * Why ?
+         * How ?
+         * When ?
+         * 
          * 
          * 
          * 
