bc3f9aa4386b19b6a1b038dc3c66b8f9d911c09e
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-09 14:32:51 +0700

    update code

--- Lipstick/Documents/Unit_3.cs
+++ Lipstick/Documents/Unit_3.cs
@@ -2,7 +2,7 @@
 {
     public class Unit_3
     {
-        /* Buoc 0: Phaidung FORMAT LAI CAC TRANG WEB
+        /* 
          * Buoc 1: Minh se tao Model
          * Buoc 2: Huong dan truyen du lieu giua Controller va Model
          * Buoc 3: Su dung ViewBag, ViewData, Tempdata
