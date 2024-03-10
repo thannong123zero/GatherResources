4293201605715930a5cb82797fd1b49071a62836
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-10 16:15:42 +0700

    update code

---
+++ CRM/Controllers/ProductionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.UserInterfaceDTO;
+
+namespace CRM.Controllers
+{
+    public class ProductionController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<ProductUI> data = new List<ProductUI>() { new ProductUI()
+            {
+                ID = Guid.NewGuid(),
+                NameEN = "ABC",
+                NameVN = "abc"
+            } };
+            return View(data);
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Update()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Delete()
+        {
+            return View();
+        }
+    }
+}
