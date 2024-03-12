8bc97f613d0fc1e79c1bc58078b13e0f9a327032
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-12 08:50:23 +0700

    update code

--- Lipstick/Documents/Unit_4.cs
+++ Lipstick/Documents/Unit_4.cs
@@ -3,8 +3,12 @@
     public class Unit_4
     {
         /*
-         * Trong phan nay chung ta se hoc ve mutiple language
-         * Chung se viet kich ban de xay duong lai khoa hoc
+         * Chung ta se hoc ve controller
+         * 
+         * Va mot so middle ware
+         * 
+         * action filter
+         * 
          * 
          */
     }
