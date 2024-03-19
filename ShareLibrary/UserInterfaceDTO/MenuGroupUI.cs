ce70f8c3efb4a5c6b4590cadcc9c84b88eb5a5cc
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-03-19 22:09:56 +0700

    update code

--- ShareLibrary/UserInterfaceDTO/MenuGroupUI.cs
+++ ShareLibrary/UserInterfaceDTO/MenuGroupUI.cs
@@ -5,11 +5,14 @@ namespace SharedLibrary.UserInterfaceDTO
     public class MenuGroupUI : BaseUI
     {
         public Guid ID { get; set; }
-        [Required(ErrorMessage ="Yeu can nhap Ten Danh Muc")]
-        [MinLength(6, ErrorMessage = "minLenght")]
         public string NameEN { get; set; }
         public string NameVN { get; set; }
         public string? DiscriptionEN { get; set; }
         public string? DiscriptionVN { get; set; }
+        public MenuGroupUI()
+        {
+            ID = Guid.NewGuid();
+            IsDeleted = false;
+        }
     }
 }
