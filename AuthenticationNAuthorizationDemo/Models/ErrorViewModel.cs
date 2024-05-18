1ef249380143a9013c03337e75b70d3ef6570ab3
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-05-18 14:21:32 +0700

    update code

---
+++ AuthenticationNAuthorizationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthenticationNAuthorizationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
