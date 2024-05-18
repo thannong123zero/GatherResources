275df8aae443973041e2c70e89a84ba09888d15e
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-05-18 15:09:09 +0700

    update code

---
+++ AuthenticationNAuthorizationDemo/APIControllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationNAuthorizationDemo.APIControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
