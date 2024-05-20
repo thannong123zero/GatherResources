25204cb7bcd19bea5bb703da378c9c1fa94f4566
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-05-20 17:55:56 +0700

    update

--- AsynchronousDemo/Program.cs
+++ AsynchronousDemo/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace AsynchronousDemo
+﻿using System.Threading.Tasks;
+
+namespace AsynchronousDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //ThreadDemo();
+            Console.WriteLine("Main thread exits.");
+        }
+
+        private static void ThreadDemo()
+        {
+            // Khởi tạo hai luồng
+            Thread thread1 = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Sleep for 2 seconds.");
+                    Thread.Sleep(2000);
+                }
+            }));
+            Thread thread2 = new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Sleep for 2 seconds.");
+                    Thread.Sleep(2000);
+                }
+            }));
+
+            // Bắt đầu thực hiện hai luồng
+            thread2.Start();
+            thread1.Start();
+
+            // Đợi cho đến khi cả hai luồng hoàn thành
+            thread1.Join();
+            thread2.Join();
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
---
+++ AsynchronousDemo/document.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousDemo
+{
+    internal class document
+    {
+        /* CPU la gi trung tam xu ly cua may tinh!
+         * - Nhan trong CPU la mot bo vi xu ly (Processor). 
+         * - Bo vi xu ly nay co the chua nhieu nhan (Core).
+         * - Tat ca cac nhan ket hop voi nhau de xu ly da nhiem.
+         * - Luong CPU la so luong nhiem vu ma CPU co the xu ly cung luc.
+         * - Luong CPU la thanh phan tap hop cac trinh dieu khien va truyen du lieu xu ly.
+         * 
+         * 
+         * Lap trinh bat dong bo:
+         *  => The nao la da luong ?
+         *  + Da luong la ky thuat trong do mot tien trinh (Process) co the chua va quan ly nhieu tien trinh con (Thread) khac.
+         * 
+         *  Thread : luong
+         *  Task: Nhiem vu
+         * 
+         * 
+         */
+    }
+}
