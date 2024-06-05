881c8e57d1a4e085266a4433d2d2b8b940d4dbba
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-06-05 16:25:09 +0700

    Update code

---
+++ SignalRNotificationDemo/Documents/Documents.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SignalRNotificationDemo.Documents
+{
+    public class Documents
+    {
+        /* Hubs
+         *  
+         *  The SignalR server component is called a hub. The hub is responsible for receiving messages from clients and sending messages to clients.
+         *  The signalR hubs api enables you to call methods on connected clients from the server.
+         * 
+         */
+    }
+}
---
+++ SignalRNotificationDemo/Models/DataObjectResult.cs
@@ -0,0 +1,16 @@
+﻿namespace SignalRNotificationDemo.Models
+{
+    public class DataObjectResult
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public string? Html { get; set; }
+        public string? Object { get; set; }
+        public List<string> ArrayObject { get; set; }
+        public DataObjectResult()
+        {
+            Success = false;
+            ArrayObject = new List<string>();
+        }
+    }
+}
