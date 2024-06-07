35b70c6d28fb04cd9ea2749143c16131f8dbe0b8
TÀI NGUYỄN HOÀNG <nguyenhoangtai2k@gmail.com>
2024-06-07 14:39:15 +0700

    update code

---
+++ SignalRNotificationDemo/Services/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRNotificationDemo.Services.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendNotification(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", user, message);
+        }
+    }
+}
