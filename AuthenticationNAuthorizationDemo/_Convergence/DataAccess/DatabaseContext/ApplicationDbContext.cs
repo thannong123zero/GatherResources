567083c6294bb3a6f244dd5baef370a77832ee12
TÀI NGUYỄN HOÀNG <nguyenhoangtai2k@gmail.com>
2024-06-11 16:01:31 +0700

    update code

--- AuthenticationNAuthorizationDemo/_Convergence/DataAccess/DatabaseContext/ApplicationDbContext.cs
+++ AuthenticationNAuthorizationDemo/_Convergence/DataAccess/DatabaseContext/ApplicationDbContext.cs
@@ -2,10 +2,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace AuthenticationNAuthorizationDemo.DatabaseContext
+namespace AuthenticationNAuthorizationDemo._Convergence.DataAccess.DatabaseContext
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
 }
