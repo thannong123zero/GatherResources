bd2c7d6be72f839a4e428860b093359d0b9a432d
TÀI NGUYỄN HOÀNG <nguyenhoangtai2k@gmail.com>
2024-06-12 16:46:01 +0700

    Update code

---
+++ AuthenticationNAuthorizationDemo/wwwroot/_documents/document.cs
@@ -0,0 +1,21 @@
+﻿namespace AuthenticationNAuthorizationDemo.wwwroot._documents
+{
+    public class document
+    {
+        // Docemnetation
+        /*
+         * Authentication  is the process of determining a user's identity.
+         * Authorization is the process of determining whether a user has access to a resource.
+         * 
+         */
+        /*
+         *  Authentication concepts
+         *  
+         *  Authentication is reponsible for providing the ClaimsPrincipal for authorization to make permission deciciions against.
+         *  + Authentication scheme
+         *  + The default authentication scheme, discussed in the next two sections.
+         *  + Directly se HttpContex.User
+         * 
+         */
+    }
+}
