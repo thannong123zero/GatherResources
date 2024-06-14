e77e5b86154bb4a3841742d0bb554feddd3e4fcb
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-06-14 22:07:59 +0700

    update code

--- StockMarketAnalysis/Controllers/HomeController.cs
+++ StockMarketAnalysis/Controllers/HomeController.cs
@@ -14,11 +14,20 @@ namespace StockMarketAnalysis.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(string stockName = "VHM", DateTime? fromDate = null, DateTime? toDate = null)
         {
             StockAnalysisViewModel stock = new StockAnalysisViewModel();
-            string url = "https://restv2.fireant.vn/symbols/VHM/historical-quotes?startDate=2024-01-19&endDate=2024-05-19";
+            DateTime startDate = DateTime.Now.AddDays(-7);
+            DateTime endDate = DateTime.Now;
+            if(fromDate != null && toDate != null)
+            {
+                startDate = fromDate.Value;
+                endDate = toDate.Value;
+            }
+
+            string url = "https://restv2.fireant.vn/symbols/"+stockName.ToUpper()+"/historical-quotes?startDate="+startDate.ToString("yyyy-MM-dd")+"&endDate="+ endDate.ToString("yyyy-MM-dd");
             string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSIsImtpZCI6IkdYdExONzViZlZQakdvNERWdjV4QkRITHpnSSJ9.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4iLCJhdWQiOiJodHRwczovL2FjY291bnRzLmZpcmVhbnQudm4vcmVzb3VyY2VzIiwiZXhwIjoyMDEzNzQ2Mzc4LCJuYmYiOjE3MTM3NDYzNzgsImNsaWVudF9pZCI6ImZpcmVhbnQud2ViIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsInJvbGVzIiwiZW1haWwiLCJhY2NvdW50cy1yZWFkIiwiYWNjb3VudHMtd3JpdGUiLCJvcmRlcnMtcmVhZCIsIm9yZGVycy13cml0ZSIsImNvbXBhbmllcy1yZWFkIiwiaW5kaXZpZHVhbHMtcmVhZCIsImZpbmFuY2UtcmVhZCIsInBvc3RzLXdyaXRlIiwicG9zdHMtcmVhZCIsInN5bWJvbHMtcmVhZCIsInVzZXItZGF0YS1yZWFkIiwidXNlci1kYXRhLXdyaXRlIiwidXNlcnMtcmVhZCIsInNlYXJjaCIsImFjYWRlbXktcmVhZCIsImFjYWRlbXktd3JpdGUiLCJibG9nLXJlYWQiLCJpbnZlc3RvcGVkaWEtcmVhZCJdLCJzdWIiOiI3YjRkNGU3Ny04MGM0LTQ4MjEtODZjNy0zN2FmNGIyM2QxYjgiLCJhdXRoX3RpbWUiOjE3MTM3NDYzNzgsImlkcCI6Ikdvb2dsZSIsIm5hbWUiOiJuZ3V5ZW5ob2FuZ3RhaTJrQGdtYWlsLmNvbSIsInNlY3VyaXR5X3N0YW1wIjoiYWFkMzk0NWUtMDhhNS00ODYwLTg4NzUtOTUzNjA0Y2NhZWMzIiwianRpIjoiODM2NjliODViNDNkNzEzZTc3ZTk3NWFlZDY1YjhjOGYiLCJhbXIiOlsiZXh0ZXJuYWwiXX0.mQtBRabeioLfClx4u96us-nXSkDdcfVJps3RMDEkJ9lBVBaSbN_YihMt3sgEqQcdaYLXboveL2EYflmGicAAQyd7sgGCNI91f7a7kkUmyOU4AV8waJTgHuX5ro9QxUzhBmRnBqwTOzsg9zz8rg4S5JqSS-DR2vijGeh2GQdhZzNqv7ZhTP4uNl0rhSWaa2oM1KuWxAA5Q-PbUfZFuhdSgro1u7SFuzVtGwPZAYUlzShi9y2vehRXI_qj0hziUFk7w35fkmzx7OeyVwJFJfyplcqI_ek-WvgxVLXGrPbugFXtNmT6rf82rUJXMjTojwRFnbZA194SwIaqB_jslLFrEw";
+            
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
             HttpResponseMessage response = client.GetAsync(url).Result;
@@ -28,9 +37,21 @@ namespace StockMarketAnalysis.Controllers
                 List<HistoryViewModel> history = JsonConvert.DeserializeObject<List<HistoryViewModel>>(data);
                 stock.History = history;
             }
-
+            ViewBag.StockName = stockName;
+            ViewBag.FromDate = startDate.ToString("yyyy-MM-dd");
+            ViewBag.ToDate = endDate.ToString("yyyy-MM-dd");
+            ViewBag.Count = stock.History.Count();
+            ViewBag.NetSale = stock.History.Where(s => (s.BuyForeignValue - s.SellForeignValue) < 0).ToList().Count();
+            ViewBag.NetBuy = stock.History.Where(s => (s.BuyForeignValue - s.SellForeignValue) > 0).ToList().Count();
 
             return View(stock);
+        }
+        public IActionResult Chart()
+        {
+
+
+
+            return View();
         }
 
         public IActionResult Privacy()
