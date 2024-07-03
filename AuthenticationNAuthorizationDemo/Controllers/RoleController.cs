67bfafa214ae18e5029c17d3a20062903b11f591
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-07-03 23:37:51 +0700

    update code

---
+++ AuthenticationNAuthorizationDemo/Controllers/RoleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationNAuthorizationDemo.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string roleName)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Update()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Update(string roleName)
+        {
+            return View();
+        }
+    }
+}
