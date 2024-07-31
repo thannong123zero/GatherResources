a32c09f37b9fe7076cd1bee954279aed12d97566
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-07-31 15:00:36 +0700

    Update code

---
+++ ChartJSDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChartJSDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
