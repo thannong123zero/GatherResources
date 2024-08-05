803e96b8859e78ea68c634f0b1bcfe738318d186
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-08-05 09:45:58 +0700

    Update code

---
+++ ChartJSDemo/Controllers/PieChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChartJSDemo.Controllers
+{
+    public class PieChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
