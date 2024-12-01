10a3abf707c081492c05af1bf696c0f819d93adb
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-12-01 15:02:15 +0700

    update code

--- MultipleLanguageDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MultipleLanguageDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
