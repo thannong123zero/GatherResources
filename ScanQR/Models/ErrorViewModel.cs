c77717c43632d32cbd091243d07da1795fea0ac4
TÀI NGUYỄN HOÀNG <nguyenhoangtai2k@gmail.com>
2025-02-11 10:17:17 +0700

    update code

---
+++ ScanQR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScanQR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
